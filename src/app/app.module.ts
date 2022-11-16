fd37629ef9dfb4a32910c5146607800704165e07
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-11-16 23:46:57 +0100

    add tepmlate and form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './forms/templateDriven-Form/templateDriven-Form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forms/templateDriven-Form/templateDriven-Form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templateDriven-Form',
+  templateUrl: './templateDriven-Form.component.html',
+  styleUrls: ['./templateDriven-Form.component.css']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
