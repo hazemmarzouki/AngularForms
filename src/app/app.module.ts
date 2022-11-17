3edaf712d194f7cb5852b0053910797e2414a0fe
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-11-17 16:49:13 +0100

    edit styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TemplateDrivenFormComponent } from './forms/templateDriven-Form/templateDriven-Form.component';
+import { ContactReactiveFormComponent } from './forms/contactReactive-Form/contactReactive-Form.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ContactService } from './services/contact.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent
+    TemplateDrivenFormComponent,
+    ContactReactiveFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
    
   ],
-  providers: [],
+  providers: [
+    ContactService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
